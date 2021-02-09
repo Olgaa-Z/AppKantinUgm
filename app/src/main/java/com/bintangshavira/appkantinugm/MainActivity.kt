3757f7004922f176f1c175e6034f26e879f6ecb1
zelviaolgamaharani <zelviaolgamaharani98@gmail.com>
2021-02-08 18:02:04 -0800

    Initial commit

---
+++ app/src/main/java/com/bintangshavira/appkantinugm/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.bintangshavira.appkantinugm
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
